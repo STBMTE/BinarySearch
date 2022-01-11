ffefe7a97bc8a91b61a78b025d29739feadb5efe
User131124 <46863300+User131124@users.noreply.github.com>
2022-01-11 16:01:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ShakerSort
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Random random = new Random();
+            int[] vs = new int[100];
+            for (int i = 0; i < 100; i++)
+            {
+                vs[i] = i * 3;
+            }
+            BinarySearch bs = new BinarySearch();
+            for (int i = 0; i < 100; i++)
+            {
+                Console.WriteLine(bs.Search(vs, vs[i]));
+            }
+            
+        }
+    }
+
+    class BinarySearch
+    {
+        public int Search(int[] Array, int SearchedNumber)
+        {
+            int StartSearchId = 0;
+            int SearchedId = -1;
+            int length = Array.Length / 2;
+            int lengthStep = Array.Length / 2;
+            for(int i = 0; i < Array.Length; i++)
+            {
+                if(lengthStep < 2)
+                {
+                    lengthStep = 2;
+                }
+                if (Array[length] == SearchedNumber)
+                {
+                    return length;
+                }
+                if(Array[length] < SearchedNumber)
+                {
+                    lengthStep /= 2;
+                    length += lengthStep;
+                }
+                if(Array[length] > SearchedNumber)
+                {
+                    lengthStep /= 2;
+                    length -= lengthStep;
+                }
+            }
+            return SearchedId;
+        }
+    }
+}
